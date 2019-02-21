fc9b4106f4d18c2cc8d88c144ea30876b928625e
caterinamennito <caterina.mennito@gmail.com>
2019-02-21 13:20:03 +0100

    complet

---
+++ assets/Js/script_accueil.js
@@ -0,0 +1,41 @@
+(function() {
+
+
+window.onload= getName();
+
+function getName() {
+  var n, q = 'Quel est ton prénom?';
+  if (!window.sessionStorage) { // if sessionStorage not supported
+  return window.prompt(q); // perform other action
+  }
+  n = window.sessionStorage.getItem('name');
+
+  if (!n) {
+      n = window.prompt(q);
+      window.sessionStorage.setItem('name', n);
+  }
+  //document.getElementById("result").innerHTML = "Bienvenue " + n ;
+  //return n;
+
+  var i = 0;
+  var speed = 70;
+  var txt =  "Bienvenue " + n ;
+
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("result").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+
+    typeWriter(txt, speed);
+
+
+  }
+
+
+
+
+    })();
---
+++ assets/Js/script_restos.js
@@ -0,0 +1,63 @@
+(function() {
+
+
+
+function aujourdHui() {
+      var d = new Date();
+      var y = d.getFullYear();
+      var m = d.getMonth();
+      var j = d.getDate();
+      var s = d.getDay();
+      var h = d.getHours();
+      var min = d.getMinutes();
+      var jours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
+      var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
+
+      var date = jours[s] + " "+ j + " " + mois[m] + " " + y;
+      var heure = h + "h" + min;
+
+
+
+  function ouverture(){
+
+      if (j>0){
+        if (h >=12 && h<=21){
+          var ouv = "OUVERT";
+          return ouv;
+        }
+
+        else if( h === 11 || h===22 ){
+        if(min>=30){
+          var ouv = "OUVERT";
+          return ouv;
+          }
+        else {
+          var ouv = "FERMÉ";
+          return ouv;        }
+      }
+
+      else {
+        var ouv = "FERMÉ";
+        return ouv;
+      }
+      }
+    }
+
+var ouverture = ouverture();
+console.log(ouverture);
+
+  document.getElementById('date').innerHTML ="Nous sommes le " + date +" , " + heure + "<br>" + "Le restaurant est donc " + ouverture;
+  document.getElementById('dateG').innerHTML ="Nous sommes le " + date +" , " + heure + "<br>" + "Le restaurant est donc " + ouverture;
+  document.getElementById('dateL').innerHTML ="Nous sommes le " + date +" , " + heure + "<br>" + "Le restaurant est donc " + ouverture;
+
+
+
+
+
+
+  };
+
+
+aujourdHui();
+
+    })();
